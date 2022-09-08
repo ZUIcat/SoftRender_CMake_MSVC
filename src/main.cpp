3083b96629422d30582be7c7aba0c7a1ef93c059
ZUIcat <35529568+ZUIcat@users.noreply.github.com>
2022-09-08 19:03:33 +0800

    none

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,6 @@
 int main(int argc, char *argv[]) {
     std::cout << "[APP END]" << std::endl;
 
-    class MyStruct {
-      //public:
-      //  explicit MyStruct() = default;
-      //  virtual ~MyStruct() = default;
-
-      private:
-        float top;    // 顶长
-        float bottom; // 底长
-    };
-
-    MyStruct m;
-    MyStruct m2{};
-
-    // https://zh.cppreference.com/w/cpp/language/initialization
-    // https://stackoverflow.com/questions/9515327/default-constructors-initialization-of-pod-and-implicit-type-conversions-in-c
-    // https://stackoverflow.com/questions/2417065/does-the-default-constructor-initialize-built-in-types
-    // https://stackoverflow.com/questions/3931312/value-initialization-and-non-pod-types/3931589
-
     MyWindow window;
     window.init();
     window.startLoop();
